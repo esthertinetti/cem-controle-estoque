d58b6e1dc8303f7b97d33ae9ecb66c63cce8a658
Esther Valerio Tinetti <esther.tinetti@dev.auaha.com.br>
2025-04-10 21:48:51 -0300

    add build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/cem-controle-estoque/', // MUITO IMPORTANTE
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
