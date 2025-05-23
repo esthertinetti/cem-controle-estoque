b23bd9258f77d6f5cc48534ef79e2e96e37191a9
Esther Tinetti <esthertinetti@gmail.com>
2025-05-22 22:25:03 -0300

    feat: git pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Routes from './routes';
 
 function App() {
   return (
-    <BrowserRouter basename="/cem-controle-estoque">
+    <HashRouter>
       <Routes />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
